a0a832797b17237f5973360f4ca4ceb4f5b3ad81
izzatillotilavoldiyev5 <izzatillotilavoldiyev5@gmail.com>
2023-09-19 10:45:57 +0500

    some logics updated

--- src/main/java/com/tillo/springbootredis/config/RedisConfig.java
+++ src/main/java/com/tillo/springbootredis/config/RedisConfig.java
@@ -35,7 +35,6 @@ public class RedisConfig {
         redisTemplate.setEnableTransactionSupport(true);
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
-
     }
 
 }
--- src/main/java/com/tillo/springbootredis/redisJPA/service/user/UserServiceImpl.java
+++ src/main/java/com/tillo/springbootredis/redisJPA/service/user/UserServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -34,7 +35,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUserById(Long userID) {
-        return userRepository.findById(userID).get();
+        Optional<User> user = userRepository.findById(userID);
+        return user.orElse(null);
     }
 
     @Override
